6383222c6026492687fc74e2a80b7fc26c034ff1
Eugene Kravchenko <yevhen_kravchenko@epam.com>
2024-03-17 22:14:13 +0200

    added e2e tests

---
+++ jest.e2e.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  ...require('./jest.config'),
+  testMatch: ['<rootDir>/__tests__/e2e/**/*.test.ts']
+};
--- src/services/public-holidays.integration.test.ts
+++ src/services/public-holidays.integration.test.ts
@@ -8,12 +8,18 @@ import { SUPPORTED_COUNTRIES } from "../config";
 describe("public-holidays.service", () => {
     const year = new Date().getFullYear();
     const country = SUPPORTED_COUNTRIES[0];
-    
+
     describe('getListOfPublicHolidays', () => {
         it('returns a list of public holidays for a valid year and country', async () => {
             const holidays = await getListOfPublicHolidays(year, country);
             
-            expect(holidays).not.toHaveLength(0);
+            expect(Array.isArray(holidays)).toBe(true);
+            expect(holidays.length).toBeGreaterThan(0);
+            holidays.forEach((holiday) => {
+                expect(holiday).toHaveProperty('date')
+                expect(holiday).toHaveProperty('localName')
+                expect(holiday).toHaveProperty('date')
+            });
         });
     
         it('should throw error if incorrect year is provided', async () => {
@@ -34,9 +40,41 @@ describe("public-holidays.service", () => {
     })
 
     describe("checkIfTodayIsPublicHoliday", () => {
+        it('returns true when today is a public holiday', async () => {
+            const result = await checkIfTodayIsPublicHoliday(country);
 
+            if (result) {
+                expect(result).toBeTruthy();
+            }
+        });
+    
+        it('properly handles validation errors if an incorrect country is provided', () => {
+            const invalidCountry = country + "S";
+
+            expect(async () => {
+                await checkIfTodayIsPublicHoliday(invalidCountry);
+            }).rejects.toThrow(`Country provided is not supported, received: ${invalidCountry}`);
+        });
     })
     describe("getNextPublicHolidays", () => {
+        it('returns a list of next public holidays for a valid year and country', async () => {
+            const holidays = await getNextPublicHolidays(country);
 
+            expect(Array.isArray(holidays)).toBe(true);
+            expect(holidays.length).toBeGreaterThan(0);
+            holidays.forEach((holiday) => {
+                expect(holiday).toHaveProperty('date')
+                expect(holiday).toHaveProperty('localName')
+                expect(holiday).toHaveProperty('date')
+            });
+        });
+    
+        it('properly handles validation errors if an incorrect country is provided', () => {
+            const invalidCountry = country + "S";
+
+            expect(async () => {
+                await getNextPublicHolidays(invalidCountry);
+            }).rejects.toThrow(`Country provided is not supported, received: ${invalidCountry}`);
+        });
     })
 })
