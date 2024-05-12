d86600b752756633a9680e20c24137b03850d1fa
ekravchenkoepam <yevhen_kravchenko@epam.com>
2024-05-12 21:35:28 +0300

    updated scripts

--- src/logger.ts
+++ src/logger.ts
@@ -13,3 +13,16 @@ export const logger = winston.createLogger({
         new winston.transports.Console()
     ],
 });
+
+switch (process.env.NODE_ENV) {
+    case 'production':
+        logger.level = 'info';
+    case 'development':
+    case 'test':
+        logger.level = 'debug';
+        break;
+    default:
+        logger.warn(`Unrecognized environment: ${process.env.NODE_ENV}`);
+        logger.level = 'info'; 
+        break;
+}
